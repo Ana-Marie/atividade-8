2f54851c37280152f69bd3a56d63cf0a274de96b
Ana-Marie <anamarie1907@gmail.com>
2021-11-01 09:31:19 -0300

    identificando side efect functions

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const document = new DOMParser().parseFromString(fs.readFileSync('res/f-droid.xm
 const isAddedAfter2018AndUpdatedAfter2019 = isValid(R.__, 2018, 2019);
 
 const addedApps = elementsToArray(document.getElementsByTagName('application'))
-    .filter(isAddedAfter2018AndUpdatedAfter2019)
-    .map(getGitHubProject);
+    .filter(isAddedAfter2018AndUpdatedAfter2019)//função com side effects em objetos
+    .map(getGitHubProject);// função com side effects em objetos
 
 console.log(addedApps.join('\n'));
  
